dc08f10be3237fcdd0ac0d272b3b26fea80f0385
e <msamisami1112@gmail.com>
2024-09-20 12:46:11 +0500

    Increment Decrement buttons applied on food carts

--- src/components/foodDisplay.jsx
+++ src/components/foodDisplay.jsx
@@ -1,20 +1,48 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { StoreContext } from "../context/StoreContext";
 import "../componentCSS/foodDisplay.css";
 import { assets } from "../assets/assets";
 
 const FoodDisplay = (category) => {
   const { food_list } = useContext(StoreContext);
+
   return (
     <>
       <div className="food-display" id="f-display">
         <h2>Top dishes near you</h2>
         <div className="food-list">
           {food_list.map((item, index) => {
+            const [itemCount, setItemCount] = useState(0);
             return (
               <div key={index} className="food-display-container">
                 <div className="food-image">
-                  <img src={item.image} alt={item.name} />
+                  <img
+                    className="food-image-container"
+                    src={item.image}
+                    alt={item.name}
+                  />
+                  {!itemCount ? (
+                    <img
+                      className="add-button"
+                      onClick={() => setItemCount((prev) => prev + 1)}
+                      src={assets.add_icon_white}
+                      alt="Add"
+                    />
+                  ) : (
+                    <div className="food-counter">
+                      <img
+                        onClick={() => setItemCount((prev) => prev - 1)}
+                        src={assets.remove_icon_red}
+                        alt="Remove"
+                      />
+                      <p>{itemCount}</p>
+                      <img
+                        onClick={() => setItemCount((prev) => prev + 1)}
+                        src={assets.add_icon_green}
+                        alt="Add"
+                      />
+                    </div>
+                  )}
                 </div>
                 <div className="food-info">
                   <div className="food-rating">
