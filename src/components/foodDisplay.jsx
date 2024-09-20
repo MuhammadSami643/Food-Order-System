1cbe948a1c98766275753b225aef6cae08cc2c44
e <msamisami1112@gmail.com>
2024-09-21 02:25:49 +0500

    food which is displaying on home page NOW filtering on the base of category

--- src/components/foodDisplay.jsx
+++ src/components/foodDisplay.jsx
@@ -1,59 +1,62 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { StoreContext } from "../context/StoreContext";
-import "../componentCSS/foodDisplay.css";
 import { assets } from "../assets/assets";
+import "../componentCSS/foodDisplay.css";
 
-const FoodDisplay = (category) => {
+const FoodDisplay = ({ category }) => {
   const { food_list } = useContext(StoreContext);
+  const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
 
   return (
     <>
       <div className="food-display" id="f-display">
         <h2>Top dishes near you</h2>
         <div className="food-list">
-          {food_list.map((item, index) => {
-            const [itemCount, setItemCount] = useState(0);
-            return (
-              <div key={index} className="food-display-container">
-                <div className="food-image">
-                  <img
-                    className="food-image-container"
-                    src={item.image}
-                    alt={item.name}
-                  />
-                  {!itemCount ? (
+          {food_list.map((item, id) => {
+            // const [itemCount, setItemCount] = useState(0); -> This is also a way to change state(only state)
+            if (category === "All" || category === item.category) {
+              return (
+                <div key={id} className="food-display-container">
+                  <div className="food-image">
                     <img
-                      className="add-button"
-                      onClick={() => setItemCount((prev) => prev + 1)}
-                      src={assets.add_icon_white}
-                      alt="Add"
+                      className="food-image-container"
+                      src={item.image}
+                      alt={item.name}
                     />
-                  ) : (
-                    <div className="food-counter">
+                    {!cartItems[id] ? (
                       <img
-                        onClick={() => setItemCount((prev) => prev - 1)}
-                        src={assets.remove_icon_red}
-                        alt="Remove"
-                      />
-                      <p>{itemCount}</p>
-                      <img
-                        onClick={() => setItemCount((prev) => prev + 1)}
-                        src={assets.add_icon_green}
+                        className="add-button"
+                        onClick={() => addToCart(id)}
+                        src={assets.add_icon_white}
                         alt="Add"
                       />
-                    </div>
-                  )}
-                </div>
-                <div className="food-info">
-                  <div className="food-rating">
-                    <p>{item.name}</p>
-                    <img src={assets.rating_starts} alt="Error" />
+                    ) : (
+                      <div className="food-counter">
+                        <img
+                          onClick={() => removeFromCart(id)}
+                          src={assets.remove_icon_red}
+                          alt="Remove"
+                        />
+                        <p>{cartItems[id]}</p>
+                        <img
+                          onClick={() => addToCart(id)}
+                          src={assets.add_icon_green}
+                          alt="Add"
+                        />
+                      </div>
+                    )}
                   </div>
-                  <p>{item.description}</p>
-                  <h3>${item.price}</h3>
+                  <div className="food-info">
+                    <div className="food-rating">
+                      <p>{item.name}</p>
+                      <img src={assets.rating_starts} alt="Error" />
+                    </div>
+                    <p>{item.description}</p>
+                    <h3>${item.price}</h3>
+                  </div>
                 </div>
-              </div>
-            );
+              );
+            }
           })}
         </div>
       </div>
--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -1,11 +1,33 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets";
 
 const StoreContext = createContext(null);
 
 const StoreContextProvidor = (props) => {
+  const [cartItems, setCartItems] = useState({});
+
+  const addToCart = (itemid) => {
+    if (!cartItems[itemid]) {
+      setCartItems((prev) => ({ ...prev, [itemid]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [itemid]: prev[itemid] + 1 }));
+    }
+  };
+
+  const removeFromCart = (itemid) => {
+    setCartItems((prev) => ({ ...prev, [itemid]: prev[itemid] - 1 }));
+  };
+
+  useEffect(() => {
+    console.log(cartItems);
+  }, [cartItems]);
+
   const storeContextValue = {
     food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart,
   };
 
   return (
